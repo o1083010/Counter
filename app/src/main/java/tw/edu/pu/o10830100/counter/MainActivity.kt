73672a391aa11354e7a6e685534d45df9fde0911
o1083010 <o1083010@gm.pu.edu.tw>
2021-03-22 11:43:42 +0800

    20210322早上按鈕事件處理

---
+++ app/src/main/java/tw/edu/pu/o10830100/counter/MainActivity.kt
@@ -0,0 +1,39 @@
+package tw.edu.pu.o10830100.counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity(){
+
+    var counter:Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun AddOne(v: View){
+        counter++
+        txv.text = counter.toString()
+    }
+    fun AddTwo(v: View){
+        //counter = counter = + 2
+        counter += 2
+        txv.text = counter.toString()
+    }
+    fun Add(v: View){
+        if(v == btn1) {
+            counter++
+        }
+        else if(v==btn2){
+            counter+=2
+        }
+        else{
+            counter=0
+        }
+        txv.text = counter.toString()
+    }
+}
